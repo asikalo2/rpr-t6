15edd627d5653ade78e61cfb19dd221e46efd2c2
asikalo2 <asikalo2@etf.unsa.ba>
2018-11-25 21:41:39 +0100

    Ispravila sam fx:id-eve. Sada se forma prikazuje kada se pokrene aplikacija.

--- src/ba/unsa/etf/rpr/tutorijal06/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ba/unsa/etf/rpr/tutorijal06/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ba/unsa/etf/rpr/tutorijal06/sample.fxml"));
+        primaryStage.setTitle("Forma");
+        primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
     }
 
